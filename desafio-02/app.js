a6663fd468d0a6ec148696444a5bdce193e6f30b
Marco Santana <santannacostamarco@gmail.com>
2020-02-02 21:04:41 -0200

    challenge-2 solved

--- desafio-02/app.js
+++ desafio-02/app.js
@@ -1,6 +1,15 @@
 new Vue({
-    el: '#desafio',
-    data: {
-        valor: ''
+  el: "#desafio",
+  data: {
+    valor: "1"
+  },
+  methods: {
+    handleKeydown: function(e) {
+      this.valor = e.target.value;
+      console.log(this.valor);
+    },
+    handleClick: function() {
+      alert("test");
     }
-})
+  }
+});
